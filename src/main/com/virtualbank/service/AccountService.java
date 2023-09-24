720d237d9268389e73280b6816b731cb0a33c2bd
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2023-09-24 11:21:08 +0800

    extend interface with new method forms

--- src/main/com/virtualbank/service/AccountService.java
+++ src/main/com/virtualbank/service/AccountService.java
@@ -7,11 +7,25 @@ public interface AccountService {
 
     void deposit(String username, String password, double amount);
 
+    void deposit(AccountHolder account, double amount);
+
     void withdraw(String username, String password, double amount);
+
+    void withdraw(AccountHolder account, double amount);
 
     void transfer(String fromUsername, String password, String toUsername, double amount);
 
+    void transfer(AccountHolder account, String toUsername, double amount);
+
     double getAccountBalance(String name);
 
+    double getAccountBalance(AccountHolder account);
+
+    void checkIfUserExists(String username);
+
     AccountHolder login(String username, String password);
+
+    void showLogs(AccountHolder account);
+
+    void authenticate(AccountHolder account, String password);
 }
